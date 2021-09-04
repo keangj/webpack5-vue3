091299d3db82159b608e085558d0c380b8ddd1b1
jaykang <keangjay@gmail.com>
2021-09-05 01:44:00 +0800

    添加 @ 别名

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import '@/test'
 const str = 'hi'
 const foo = (str: string) => {
     console.log(str)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/')
+    },
+    extensions: [".json", ".ts", ".js", "..."]
+  },
   module: {
     rules: [
       {
